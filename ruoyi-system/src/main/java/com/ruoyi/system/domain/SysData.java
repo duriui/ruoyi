b7e1878b66cccaec826057b947e552e18a01b205
AIIT1677DR <ddddurui@ustc.edu>
2024-04-29 18:25:17 +0800

    updata 新增一种数据

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SysData.java
@@ -0,0 +1,95 @@
+package com.ruoyi.system.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SysData {
+
+    private String configName;
+
+//    private String column1;
+//
+//    private String column2;
+//
+//    private String column3;
+//
+//    private String column4;
+//
+//    private String column5;
+//
+//    private String column6;
+//
+//    private String column7;
+//
+//    private String column8;
+//
+//    private String column9;
+//
+//    private String column10;
+//
+//    private String column11;
+//
+//    private String column12;
+//
+//    private String column13;
+//
+//    private String column14;
+//
+//    private String column15;
+//
+//    private String column16;
+//
+//    private String column17;
+//
+//    private String column18;
+//
+//    private String column19;
+//
+//    private String column20;
+//
+//    private String data1;
+//
+//    private String data2;
+//
+//    private String data3;
+//
+//    private String data4;
+//
+//    private String data5;
+//
+//    private String data6;
+//
+//    private String data7;
+//
+//    private String data8;
+//
+//    private String data9;
+//
+//    private String data10;
+//
+//    private String data11;
+//
+//    private String data12;
+//
+//    private String data13;
+//
+//    private String data14;
+//
+//    private String data15;
+//
+//    private String data16;
+//
+//    private String data17;
+//
+//    private String data18;
+//
+//    private String data19;
+//
+//    private String data20;
+
+
+}
